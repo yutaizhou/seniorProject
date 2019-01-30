443e4c51ad56d5749c0cf1a2e042b622392783fc
Yutai <yutai.20m5@gmail.com>
2019-01-30 08:35:19 -0500

    binary image threshold, refactored visualization

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from util import *
 cmap = 'gray'
 
-# load data, grayscale, crop
-img_og = cv.imread('data/test_2.png')
-img_gray = cv.cvtColor(img_og,cv.COLOR_BGR2GRAY)
-img = crop_borders(img_gray)
+# load data, grayscale then binary, crop
+img_gray = cv2.imread('data/test_1.png', cv2.IMREAD_GRAYSCALE)
+(thresh, im_bw) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+img = crop_borders(im_bw)
 
 # erosion
-erosion_type = cv.MORPH_ELLIPSE
+erosion_type = cv2.MORPH_ELLIPSE
 erosion_size = 4
-kernel = cv.getStructuringElement(erosion_type, (2*erosion_size + 1, 2*erosion_size+1), (erosion_size, erosion_size))
-img_eroded = cv.dilate(img, kernel)
+kernel = cv2.getStructuringElement(erosion_type, (2*erosion_size + 1, 2*erosion_size+1), (erosion_size, erosion_size))
+img_eroded = cv2.dilate(img, kernel)
 
-plt.subplot(1,2,1)
-plt.imshow(img, cmap)
-plt.subplot(1,2,2)
-plt.imshow(img_eroded, cmap)
+# opening
+img_open = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+# visualization
+subplot_rows = 1
+subplot_cols = 3
+images = [img, img_eroded, img_open]
+for index, image in enumerate(images):
+	fig = plt.subplot(subplot_rows,subplot_cols,index+1)
+	plt.imshow(image, cmap)
+	fig.axes.get_yaxis().set_ticks([])
+	fig.axes.get_xaxis().set_ticks([])
 plt.show()
